38ac8cf2918d6ec82b78a58b3c171d11fa606b7c
rhernandez <rhernandez@MX1198>
2016-08-02 10:15:36 -0500

    Add project files.

---
+++ frmGetUUIDfromXML/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace frmGetUUIDfromXML
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.btnGo = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.txtPath = new System.Windows.Forms.TextBox();
+      this.lblMessage = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.btnCopyFilesByList = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnGo
+      // 
+      this.btnGo.Image = ((System.Drawing.Image)(resources.GetObject("btnGo.Image")));
+      this.btnGo.Location = new System.Drawing.Point(50, 31);
+      this.btnGo.Name = "btnGo";
+      this.btnGo.Size = new System.Drawing.Size(157, 186);
+      this.btnGo.TabIndex = 0;
+      this.btnGo.Text = "GO!";
+      this.btnGo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+      this.btnGo.UseVisualStyleBackColor = true;
+      this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(9, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(33, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Ruta:";
+      // 
+      // txtPath
+      // 
+      this.txtPath.Location = new System.Drawing.Point(47, 5);
+      this.txtPath.Name = "txtPath";
+      this.txtPath.Size = new System.Drawing.Size(305, 20);
+      this.txtPath.TabIndex = 2;
+      // 
+      // lblMessage
+      // 
+      this.lblMessage.AutoSize = true;
+      this.lblMessage.Location = new System.Drawing.Point(12, 101);
+      this.lblMessage.Name = "lblMessage";
+      this.lblMessage.Size = new System.Drawing.Size(32, 13);
+      this.lblMessage.TabIndex = 3;
+      this.lblMessage.Text = "Listo!";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(277, 31);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Retenciones";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // btnCopyFilesByList
+      // 
+      this.btnCopyFilesByList.Location = new System.Drawing.Point(47, 336);
+      this.btnCopyFilesByList.Name = "btnCopyFilesByList";
+      this.btnCopyFilesByList.Size = new System.Drawing.Size(160, 23);
+      this.btnCopyFilesByList.TabIndex = 5;
+      this.btnCopyFilesByList.Text = "copy files by list";
+      this.btnCopyFilesByList.UseVisualStyleBackColor = true;
+      this.btnCopyFilesByList.Click += new System.EventHandler(this.btnCopyFilesByList_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(370, 371);
+      this.Controls.Add(this.btnCopyFilesByList);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.lblMessage);
+      this.Controls.Add(this.txtPath);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnGo);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnGo;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox txtPath;
+    private System.Windows.Forms.Label lblMessage;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button btnCopyFilesByList;
+  }
+}
+
---
+++ frmGetUUIDfromXML/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace frmGetUUIDfromXML
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    //----------------------------------------------------------------------------------------------------
+    private void btnGo_Click(object sender, EventArgs e)
+    {
+      string msj;
+      msj = "Espere...";
+      lblMessage.Text = msj;
+
+      if (txtPath.Text.Trim() != string.Empty)
+      {
+        StringBuilder sb = new StringBuilder();
+        DirectoryInfo dir = new DirectoryInfo(txtPath.Text.Trim());
+        FileInfo[] files;
+
+        files = dir.GetFiles("*.xml");
+
+        foreach (FileInfo file in files)
+        {
+          sb.Append(ParseXml(file) + "\r\n");
+          //sb.Append(File.ReadAllText(file.FullName).Replace('|',','));
+        }
+
+        TextWriter sw = new StreamWriter(txtPath.Text.Trim() + "\\" + DateTime.Now.ToString("yyyyMMddhhmmss") + "_UUIDList.csv", false, Encoding.GetEncoding(1252), 512);
+        sw.Write(sb.ToString());
+        sw.Close();
+        msj = "Done!";
+      }
+      else
+      {
+        msj = "Seleccione una ruta válida";
+      }
+
+      lblMessage.Text = msj;
+    }
+
+    //----------------------------------------------------------------------------------------------------
+    private static string ParseXml(FileInfo file)
+    {
+      //H1-DATOS GENERALES DE LOS CFDI´S
+
+      XNamespace cfdi = "http://www.sat.gob.mx/cfd/3";
+      XNamespace nomina = "http://www.sat.gob.mx/nomina";
+      XNamespace tfd = "http://www.sat.gob.mx/TimbreFiscalDigital";
+
+      XElement root = XElement.Load(file.FullName);
+      //var comprobante = (from c in root.Elements(cfdi + "Comprobante") select c).FirstOrDefault();
+
+      var TimbreFiscalDigital = (from c in root.Elements(cfdi + "Complemento").Elements(tfd + "TimbreFiscalDigital") select c).FirstOrDefault();
+
+      string UUID = TimbreFiscalDigital.Attribute("UUID").Value; //0
+      string FechaTimbrado = TimbreFiscalDigital.Attribute("FechaTimbrado").Value; //0
+
+      var Emisor = (from c in root.Elements(cfdi + "Emisor") select c).FirstOrDefault();
+      string rfcEmisor = Emisor.Attribute("rfc").Value;
+
+      var Receptor = (from c in root.Elements(cfdi + "Emisor") select c).FirstOrDefault();
+      string rfcReceptor = Receptor.Attribute("rfc").Value;
+
+      //var Base = (from c in root select c).FirstOrDefault();
+      string serie = root.Attribute("serie").Value;
+      string folio = root.Attribute("folio").Value;
+      string subTotal = root.Attribute("subTotal").Value;
+      string Total = root.Attribute("total").Value;
+
+      return FechaTimbrado + "," + UUID + "," + rfcEmisor + "," + rfcReceptor + "," + serie + "," + folio + "," + subTotal + "," + Total;
+    }
+    //---------------------------------------------------------------------------------------------------------
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string msj;
+      msj = "Espere...";
+      lblMessage.Text = msj;
+
+      if (txtPath.Text.Trim() != string.Empty)
+      {
+        StringBuilder sb = new StringBuilder();
+        DirectoryInfo dir = new DirectoryInfo(txtPath.Text.Trim());
+        FileInfo[] files;
+
+        files = dir.GetFiles("*.xml");
+
+        foreach (FileInfo file in files)
+        {
+          sb.Append(ParseXmlRetenciones(file) + "\r\n");
+          //sb.Append(File.ReadAllText(file.FullName).Replace('|',','));
+        }
+
+        TextWriter sw = new StreamWriter(txtPath.Text.Trim() + "\\" + DateTime.Now.ToString("yyyyMMddhhmmss") + "_UUIDList.csv", false, Encoding.GetEncoding(1252), 512);
+        sw.Write(sb.ToString());
+        sw.Close();
+        msj = "Done!";
+      }
+      else
+      {
+        msj = "Seleccione una ruta válida";
+      }
+
+      lblMessage.Text = msj;
+    }
+    //----------------------------------------------------------------------------------------------------
+    private static string ParseXmlRetenciones(FileInfo file)
+    {
+      //H1-DATOS GENERALES DE LOS CFDI´S
+
+      XNamespace cfdi = "http://www.sat.gob.mx/cfd/3";
+      XNamespace nomina = "http://www.sat.gob.mx/nomina";
+      XNamespace tfd = "http://www.sat.gob.mx/TimbreFiscalDigital";
+      XNamespace retenciones = "http://www.sat.gob.mx/esquemas/retencionpago/1";
+
+      XElement root = XElement.Load(file.FullName);
+      //var comprobante = (from c in root.Elements(cfdi + "Comprobante") select c).FirstOrDefault();
+
+      var TimbreFiscalDigital = (from c in root.Elements(retenciones + "Complemento").Elements(tfd + "TimbreFiscalDigital") select c).FirstOrDefault();
+
+      string UUID = TimbreFiscalDigital.Attribute("UUID").Value; //0
+      string FechaTimbrado = TimbreFiscalDigital.Attribute("FechaTimbrado").Value; //0
+
+      var Emisor = (from c in root.Elements(retenciones + "Emisor") select c).FirstOrDefault();
+      string rfcEmisor = Emisor.Attribute("RFCEmisor").Value;
+
+      var Receptor = (from c in root.Elements(retenciones + "Receptor").Elements(retenciones + "Nacional") select c).FirstOrDefault();
+      string rfcReceptor = Receptor.Attribute("RFCRecep").Value;
+
+      return FechaTimbrado + "," + UUID + "," + rfcEmisor + "," + rfcReceptor;
+    }
+
+    private void btnCopyFilesByList_Click(object sender, EventArgs e)
+    {
+      string msj;
+      msj = "Espere...";
+      lblMessage.Text = msj;
+
+      int coincidences = 0;
+
+      List<string> theList = getList();
+      StringBuilder sb = new StringBuilder();
+
+      DirectoryInfo dir = new DirectoryInfo(@"C:\Users\rhernandez.TURIN\Desktop\TAR\");
+      FileInfo[] files;
+
+      files = dir.GetFiles("*.xml", SearchOption.AllDirectories);
+
+      foreach (FileInfo file in files)
+      {
+        foreach (string folio in theList)
+        {
+          if (fileFieldContains(file, folio))
+          {
+            file.CopyTo(@"C:\Users\rhernandez.TURIN\Desktop\Nueva carpeta\TAR\" + file.Name);
+            sb.Append(file.Name + "," + folio + Environment.NewLine);
+            coincidences++;
+          }
+        }
+      }
+      TextWriter sw = new StreamWriter(@"C:\Users\rhernandez.TURIN\Desktop\Nueva carpeta\TAR\" + DateTime.Now.ToString("yyyyMMddhhmmss") + "_GE_List.csv", false, Encoding.GetEncoding(1252), 512);
+      sw.Write(sb.ToString());
+      sw.Close();
+      lblMessage.Text = "Done! " + coincidences;
+    }
+    //----------------------------------------------------------------------
+    private bool fileFieldContains(FileInfo file, string _folio)
+    {
+      //H1-DATOS GENERALES DE LOS CFDI´S
+
+      XNamespace cfdi = "http://www.sat.gob.mx/cfd/3";
+      XNamespace nomina = "http://www.sat.gob.mx/nomina";
+      XNamespace tfd = "http://www.sat.gob.mx/TimbreFiscalDigital";
+
+      XElement root = XElement.Load(file.FullName);
+      //var comprobante = (from c in root.Elements(cfdi + "Comprobante") select c).FirstOrDefault();
+
+      //var TimbreFiscalDigital = (from c in root.Elements(cfdi + "Complemento").Elements(tfd + "TimbreFiscalDigital") select c).FirstOrDefault();
+
+      //string UUID = TimbreFiscalDigital.Attribute("UUID").Value; //0
+      //string FechaTimbrado = TimbreFiscalDigital.Attribute("FechaTimbrado").Value; //0
+
+      //var Emisor = (from c in root.Elements(cfdi + "Emisor") select c).FirstOrDefault();
+      //string rfcEmisor = Emisor.Attribute("rfc").Value;
+
+      //var Receptor = (from c in root.Elements(cfdi + "Emisor") select c).FirstOrDefault();
+      //string rfcReceptor = Receptor.Attribute("rfc").Value;
+
+      //var Base = (from c in root select c).FirstOrDefault();
+      //string serie = root.Attribute("serie").Value;
+      string folio = root.Attribute("folio").Value;
+      //string subTotal = root.Attribute("subTotal").Value;
+      //string Total = root.Attribute("total").Value;
+
+      return folio == _folio;
+
+    }
+    //-----------------------------------------------------------------------
+    private List<string> getList()
+    {
+      List<string> theList = new List<string>();
+      theList.Add("1600007769");
+      theList.Add("1600008381");
+      theList.Add("1600008654");
+      theList.Add("1600008655");
+      theList.Add("1600008665");
+      theList.Add("1600008757");
+      theList.Add("1600008883");
+      theList.Add("1600009059");
+      theList.Add("1600009060");
+      theList.Add("1600009069");
+      theList.Add("1600009210");
+      theList.Add("1600009211");
+      theList.Add("1600009469");
+      theList.Add("1600009498");
+      theList.Add("1600009688");
+      theList.Add("1600009896");
+      return theList;
+    }
+  }
+}
